77450749c9a667028f97938d4d4f295e48dac7eb
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-04-10 07:54:08 +0300

    feat: implement date formatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,21 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.*
+
+private const val ERROR_DAY = "Такого дня не существует"
+private const val RU = "ru"
+
 class DateFormatter {
 
-    // TODO: Complete the following function
-    fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+    fun toTextDay(day: String, month: String, year: String) = runCatching {
+        val date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
+        val monthToString = date.month.getDisplayName(TextStyle.FULL, Locale(RU))
+        val dayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale(RU))
+
+        "$day $monthToString, $dayOfWeek"
+    }.getOrElse {
+        ERROR_DAY
     }
 }
