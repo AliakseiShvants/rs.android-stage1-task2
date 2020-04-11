d49ee3352196b03add4ad449cd3217f601fe0166
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-04-11 20:07:55 +0300

    feat: implement blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,34 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.LocalDate.now
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
+
+private const val EMPTY = ""
+private const val PATTERN2 = "dd.mm.yyyy"
 
 class Blocks {
 
-    // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any = when (blockB) {
+        Int::class -> (blockA.filter { it::class == Int::class } as List<Int>).sum()
+
+        String::class -> {
+            (blockA.filter { it::class == String::class } as List<String>)
+                .toTypedArray()
+                .joinToString(separator = EMPTY) { it }
+        }
+
+        else -> {
+            val dateList = (blockA.filter { it::class == LocalDate::class } as List<LocalDate>)
+            val diffList = dateList.map { now().toEpochDay() - it.toEpochDay() }
+            val minDiff = diffList.min() ?: 0L
+
+            diffList.forEachIndexed { index, value ->
+                if (value == minDiff) {
+                    return dateList[index].format(DateTimeFormatter.ofPattern(PATTERN2))
+                }
+            }
+        }
     }
 }
