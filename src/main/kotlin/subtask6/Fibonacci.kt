69642a4ba0fe1344db14ca42b155b184d31e6ef9
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-04-11 17:00:58 +0300

    feat: implement Fibonacci

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,33 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var first: Int
+        var second: Int
+        var bit = 0
+        var x = 0
+        var prod: Int
+
+        do {
+            first = getFIByOrder(x)
+            second = getFIByOrder(x + 1)
+            prod = first * second
+
+            if (prod == n) {
+                bit = 1
+
+                break
+            }
+
+            x++
+        } while (prod < n)
+
+        return intArrayOf(first, second, bit)
+    }
+
+    private fun getFIByOrder(position: Int): Int = when (position) {
+        0 -> 0
+        1 -> 1
+        else -> getFIByOrder(position - 2) + getFIByOrder(position - 1)
     }
 }
