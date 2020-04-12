5b7f3d43b29b08207dc84ebc6de30e0f650c65e0
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-04-12 09:47:40 +0300

    feat: implement abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,9 +1,36 @@
 package subtask3
 
+private const val NO = "NO"
+private const val YES = "YES"
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val input = StringBuilder(a.toUpperCase())
+        val expected = b.toCharArray()
+
+        var index = 0
+        var result = NO
+
+        while (index < b.length) {
+            if (expected[index] != input[index]) {
+                result = NO
+                input.deleteCharAt(index)
+            } else {
+                result = YES
+                index++
+            }
+
+            if (index > input.lastIndex) {
+                if (input.length != b.length) {
+                    result = NO
+
+                    break
+                }
+
+                break
+            }
+        }
+
+        return result
     }
 }
